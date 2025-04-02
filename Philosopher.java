27518e07bb98b26acabb33a8f0a0affb02f54bf4
Justin Forseth <justin@jforseth.com>
2025-04-02 15:24:25 -0600

    Getting started

---
+++ Philosopher.java
@@ -0,0 +1,9 @@
+class Philosopher implements Runnable {
+    public Philosopher() {
+        System.out.println("Philosopher");
+    }
+
+    public void run() {
+        System.out.println("I'm a philosopher");
+    }
+}
---
+++ Table.java
@@ -0,0 +1,7 @@
+public class Table {
+    public static void main(String[] args) {
+        Philosopher p = new Philosopher();
+        Thread t = new Thread(p);
+        t.start();
+    }
+}
