b51265582164cf8df6b5a4ebfa5535bfc4c6fc5d
Justin Forseth <justin@jforseth.com>
2025-04-02 15:49:23 -0600

    Set the table maybe correctly?

--- src/Table.java
+++ src/Table.java
@@ -1,10 +1,20 @@
 package src;
 
 public class Table {
+    private Chopstick[] chopsticks = new Chopstick[5];
+
     public static void main(String[] args) {
-        private Chopstick[] chopsticks = new Chopstick[5];
+        Table table = new Table();
+        table.setTable();
+    }
+
+    public void setTable() {
         for (int i = 0; i < chopsticks.length; i++) {
             chopsticks[i] = new Chopstick();
+        }
+        for (int i = 0; i < chopsticks.length; i++) {
+            Philosopher philosopher = new Philosopher(chopsticks[i], chopsticks[(i + 1) % chopsticks.length]);
+
         }
     }
 }
